399c9fa3cb87cce145643d02c743d82747d4fd85
Mohammed Imran <124262560+Imran8190@users.noreply.github.com>
2024-04-28 11:33:07 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+CHAINS = [
+  {
+    'id': 1,
+    'Model Name':'KARUGAMANI CHAIN',
+    'Model Size':'24 inches,34 inches',
+    'Cost':'Rs.400,600'
+  },
+  {
+    'id': 2,
+    'Model Name':'SARADU CHAIN',
+    'Model Size':'24 inches,34 inches',
+    'Cost':'Rs.500,700'
+  },
+  {
+    'id': 3,
+    'Model Name':'THAAMBU KAIRU CHAIN',
+    'Model Size':'24 inches,34 inches',
+    'Cost':'Rs.600,800'
+  },
+  {
+    'id': 4,
+    'Model Name':'TENDULKAR CHAIN',
+    'Model Size':'24 inches,34 inches',
+    'Cost':'Rs.300,500'
+  }
+]
 
 @app.route("/")
 
 def bismillah():
-    return "Allahu akbar ,Allahu akbar, Allahu akbar"
+    return render_template('home.html', 
+ chains=CHAINS,company_name='NEW ROYAL GOLD COVERING')
 
 
 if (__name__)=="__main__":
